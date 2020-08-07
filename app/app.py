e4f46e5793d4ccde5cc85808814376200462b0c8
Oliver Van Dyk <n10474382@qut.edu.au>
2020-08-07 20:04:07 +1000

    Ex3

---
+++ app/app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+import nltk 
+from nltk import FreqDist 
+
+nltk.download('gutenberg') 
+from nltk.corpus import gutenberg 
+
+app = Flask(__name__)
+
+@app.route("/")
+def count_words():
+    tokens = gutenberg.words('austen-sense.txt')     
+    tokens = [word.lower() for word in tokens if word.isalpha()]
+    fdist = FreqDist(tokens)     
+    common = fdist.most_common(500)
+    words = []     
+    for word, frequency in common:         
+        words.append(word)     
+    words.sort()
+    highCount = common[0][1]
+    html = "<html><head><title>Cool</title></head><body><h1>" + words + "</h1></body></html>"
+    size = str(int(15 + fdist[word] / float(highCount) * 150)) 
+    colour = str(hex(int(0.8 * fdist[word] / \                       
+                        float(highCount) * 256**3))) 
+    colour = colour[-(len(colour) - 2):] 
+    while len(colour) < 6:     
+        colour = "0" + colour 
+
+    span = ""    
+    for word in words:
+        span = span + <span style="font-size: 16px; color: #017e22">word</span>
+    html = "<html><head><title>Cool</title></head><body><h1>" + words + "</h1></body></html>"
+    
+        
+
+
+if __name__ == "__main__":
+    app.run()
+
+import nltk from nltk import FreqDist 
+nltk.download('gutenberg') from nltk.corpus import gutenberg 
